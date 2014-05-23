37f8579380c4f2e9985c49038689cf6577ba1ded
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-05-23 17:07:45 -0400

    Create Value.java

---
+++ fix-sandbox/include/v42/Value.java
@@ -0,0 +1,24 @@
+package net.openhft.fix.include.v42;
+
+public class Value 
+{
+    protected String description;
+    protected String _enum;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    public String getEnum() {
+        return _enum;
+    }
+
+    public void setEnum(String value) {
+        this._enum = value;
+    }
+
+}
