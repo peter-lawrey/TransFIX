59e3b6085bfc2082d9f71ecb0f7de25dccdf62a0
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-05-30 22:36:32 -0400

    Update Messages.java

--- fix-sandbox/include/v42/Messages.java
+++ fix-sandbox/include/v42/Messages.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.openhft.fix.include.v42;
 
 import net.openhft.lang.collection.HugeArray;
@@ -6,13 +22,13 @@ import net.openhft.lang.collection.HugeCollections;
 public class Messages 
 {
     protected HugeArray<Message> messageArr;
-    protected int messagesSize;   
+    protected int messageSize;   
     protected int fieldSize;
     protected int groupSize;
     
     
-    public Messages setMessagesSize(int messagesSize){
-    	this.messagesSize=messagesSize;
+    public Messages setMessagesSize(int messageSize){
+    	this.messageSize=messageSize;
     	return this;
     }
     
@@ -21,8 +37,8 @@ public class Messages
         
     public HugeArray<Message> getMessage() {
         if (messageArr == null) {
-            messageArr = HugeCollections.newArray(Message.class, messagesSize);
-            for (int i=0;i<messagesSize;i++)
+            messageArr = HugeCollections.newArray(Message.class, messageSize);
+            for (int i=0;i<messageSize;i++)
             {
             	Message message = messageArr.get(i);
             	message.setFieldSize(fieldSize);
