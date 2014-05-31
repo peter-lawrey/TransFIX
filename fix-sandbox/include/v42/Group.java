1ba8b80a077bf84ce81a40c463072042b09cac96
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-05-30 22:34:54 -0400

    Update Group.java

--- fix-sandbox/include/v42/Group.java
+++ fix-sandbox/include/v42/Group.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.openhft.fix.include.v42;
 
 import net.openhft.lang.collection.HugeArray;
@@ -6,18 +21,28 @@ import net.openhft.lang.collection.HugeCollections;
 public class Group 
 {
 
-    protected HugeArray<Field> fieldOrGroup;
+    protected HugeArray<Field> field;
+    protected HugeArray<Group> group;
     protected String name;
     protected String required;
-	protected int valueSize;
+	protected int fieldSize;
+	protected int groupSize;
 
-    public Group (int valueSize){this.valueSize = valueSize;}
+    public Group setFieldSize(int fieldSize){this.fieldSize = fieldSize;return this;}
+    public Group setGroupSize(int groupSize){this.groupSize = groupSize;return this;}
     
-    public HugeArray<Field> getFieldOrGroup() {
-        if (fieldOrGroup == null) {
-            fieldOrGroup = HugeCollections.newArray(Field.class, valueSize);
+    public HugeArray<Field> getField() {
+        if (field == null) {
+        	field = HugeCollections.newArray(Field.class, fieldSize);
         }
-        return this.fieldOrGroup;
+        return this.field;
+    }
+    
+    public HugeArray<Group> getGroup() {
+        if (group == null) {
+        	group = HugeCollections.newArray(Group.class, groupSize);
+        }
+        return this.group;
     }
 
     public String getName() {
