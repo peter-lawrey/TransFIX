075db441222aa4a9206d4ba26f512c4d2e8a495c
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-06-01 16:08:06 -0400

    Create Value.java

---
+++ fix-sandbox/include/src/main/java/net/openhft/fix/include/v42/Value.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.openhft.fix.include.v42;
+
+public class Value 
+{
+    protected String description;
+    protected String _enum;//predefined OR UserDefined
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    public String getEnum() {
+        return _enum;
+    }
+
+    public void setEnum(String value) {
+        this._enum = value;
+    }
+
+}
