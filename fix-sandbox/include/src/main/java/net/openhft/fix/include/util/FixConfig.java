c51df2666e353684736d72bc42003062c8756229
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-06-01 16:11:40 -0400

    Update FixConfig.java

--- fix-sandbox/include/src/main/java/net/openhft/fix/include/util/FixConfig.java
+++ fix-sandbox/include/src/main/java/net/openhft/fix/include/util/FixConfig.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.openhft.fix.include.util;
 
 import net.openhft.fix.include.v42.Components;
