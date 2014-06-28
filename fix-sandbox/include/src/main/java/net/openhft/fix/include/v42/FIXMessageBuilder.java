65864c6e274b1dfe77a801b2fb0f21b45e2b0585
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-06-28 15:40:54 -0400

    Update FIXMessageBuilder.java

--- fix-sandbox/include/src/main/java/net/openhft/fix/include/v42/FIXMessageBuilder.java
+++ fix-sandbox/include/src/main/java/net/openhft/fix/include/v42/FIXMessageBuilder.java
@@ -16,6 +16,9 @@
 
 package net.openhft.fix.include.v42;
 
+import java.nio.ByteBuffer;
+
+import sun.nio.ch.DirectBuffer;
 import net.openhft.fix.include.util.FixConfig;
 import net.openhft.fix.include.util.FixMessagePool;
 import net.openhft.fix.include.util.FixMessagePool.FixMessageContainer;
@@ -125,11 +128,19 @@ public class FIXMessageBuilder implements Cloneable
 	
 	public static void main (String ...args) throws Exception{
 		int fixMsgCount = 5;
+		/*int SIZE = 128;    
+	    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(SIZE);
+        long addr = ((DirectBuffer) byteBuffer).address();
+        NativeBytes bytes = new NativeBytes(addr, addr + SIZE);*/
+        
 		FixMessagePool fmp = new FIXMessageBuilder().initFixMessagePool(true, fixMsgCount);
 		FixMessageContainer<FixMessage> fmc = fmp.getFixMessageContainer();
 		FixMessage fm =  fmc.getFixMessage();		
 		//fm.getField(9).setFieldData(new DirectStore("121212121".length()).bytes());
-		fm.getField(9).setName("asdjaklsdjh");
+		//bytes.writeInt(769876987);
+		fm.getField(12).getFieldData().writeUTF("StringTestoeriupwouropweiur");
+		//fm.getField(12).getFieldData().position(0);
+		//fm.getField(12).getFieldData().flip();
 		System.out.println(fm.getFixString());
 		fmp.putFixMessageContainer(fmc);
 		
