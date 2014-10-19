628afdfdcc053dda2071229190a7fd993f4dceb9
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-10-19 01:12:14 -0400

    Update TestTransFix.java

--- fix-sandbox/include/src/test/java/net/openhft/fix/include/v42/examples/TestTransFix.java
+++ fix-sandbox/include/src/test/java/net/openhft/fix/include/v42/examples/TestTransFix.java
@@ -1,5 +1,10 @@
 package net.openhft.fix.include.v42.examples;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.junit.Test;
+
 import net.openhft.fix.include.util.FixMessagePool;
 import net.openhft.fix.include.util.FixMessagePool.FixMessageContainer;
 import net.openhft.fix.include.v42.FIXMessageBuilder;
@@ -9,10 +14,8 @@ import net.openhft.fix.include.v42.FixMessageReader;
 import net.openhft.lang.io.ByteBufferBytes;
 import net.openhft.lang.io.DirectStore;
 import net.openhft.lang.io.NativeBytes;
-import org.junit.Test;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class TestTransFix {
 	
@@ -60,23 +63,121 @@ public class TestTransFix {
          		
 	}
 	
+	/**
+	 * Tests the data written from a FIX message is read back correctly.
+	 */
 	@Test
 	public void updateFixFieldForEmptyMessage(){
+		
 		int fixMsgCount = Runtime.getRuntime().availableProcessors();
 		FixMessagePool fmp = new FIXMessageBuilder().initFixMessagePool(true, fixMsgCount);
 		FixMessageContainer<FixMessage> fmc = fmp.getFixMessageContainer();
 		FixMessage fm =  fmc.getFixMessage();		
-		ByteBufferBytes bbUser = new ByteBufferBytes(ByteBuffer.allocate(1024).order(ByteOrder.nativeOrder()));
+			
+		String [] fixElements = {"8=FIX.4.2", "9=154", "35=6", "49=BRKR", "6=INVMGR", "34=238", "52=19980604-07:59:56", "23=115686", "28=N", "55=AXX.AX", "54=2", "27=250000", "44=7900.000000", "25=H", "10=231"}; 
 		
-		//assertEquals(0, fm.getField(8).getFieldData().position());
+		String [] fixData = null;
+		for (int i=0;i<fixElements.length;i++){
+			fixData = fixElements[i].split("=");
+			
+			//assert field is empty
+			assertEquals(0, fm.getField(Integer.parseInt(fixData[0])).getFieldData().position());
+			
+			fm.getField(Integer.parseInt(fixData[0])).setFieldData(fixData[1].getBytes());
+			
+			//assert field data is equal to the written data from the FixMessage.
+			assertEquals(fixData[1].length(), fm.getField(Integer.parseInt(fixData[0])).getFieldData().position());
+			
+			System.out.println(fixElements[i]+ " length written "+fixData[1].getBytes().length+ "<==>" + fm.getField( Integer.parseInt(fixData[0]) ).getFieldData().position() );
+		}
 		
-		byte[] fixHeader = "FIX.4.2".getBytes();
-		fm.getField(8).setFieldData(fixHeader);//write into FixMessage field 8		
-	//	assertEquals(fixHeader.length, fm.getField(8).getFieldData().position());
+		ByteBufferBytes reverseLookupBuf = null;
+		byte[] readBytes = null;
+		int len = -1;
+		String bufData = null;
+		for (int i=0;i<fixElements.length;i++){
+			fixData = fixElements[i].split("=");
+			
+			//reading data from ByteBufferByte back for verification
+			reverseLookupBuf = fm.getField( Integer.parseInt(fixData[0]) ).getFieldData();			
+			reverseLookupBuf.flip();
+			len = (int)(reverseLookupBuf.limit()- reverseLookupBuf.position());			
+			readBytes = new byte[(int) len];			
+			reverseLookupBuf.readFully(readBytes);
+			
+			bufData = fm.getField(Integer.parseInt(fixData[0])).getNumber()+"="+ new String (readBytes);
+			
+			//compare original data with read data from FixMessage
+			assertEquals(fixElements[i], bufData);
+			
+			System.out.println(fixElements[i]+ ": Reverse Look up-->"+ bufData);
+		}	
+	}
+	
+	/**
+	 * Test the validity (check like 8|9|34|35|49|56|10 ) of a FIX message fed to FixMessage object checked out from the pool. 
+	 * Rejects on incorrect message.
+	 */
+	@Test
+	public void fixMessageSessionLevelCheck(){
+		String validFixMessage = "8=FIX.4.2|9=154|35=6|49=BRKR|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|10=231|";
 		
-		//bbUser.write(fixVersion);//write into ByteBuffer		
+		String invalidFixMessage_1 = "9=154|35=6|49=BRKR|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		String invalidFixMessage_2 = "8=FIX.4.2|35=6|49=BRKR|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		String invalidFixMessage_3 = "8=FIX.4.2|9=154|49=BRKR|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		String invalidFixMessage_4 = "8=FIX.4.2|9=154|35=6|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		String invalidFixMessage_5 = "8=FIX.4.2|9=154|35=6|49=BRKR|34=238|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		String invalidFixMessage_6 = "8=FIX.4.2|9=154|35=6|49=BRKR|56=INVMGR|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		String invalidFixMessage_7 = "8=FIX.4.2|9=154|35=6|49=BRKR|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=AXX.AX|54=2|27=250000|44=7900.000000|25=H|";
 		
-		System.out.println(fixHeader.length+ "=?==" + fm.getField(8).getFieldData().position());
+		String fixMessagesArray [] = {validFixMessage, invalidFixMessage_1, invalidFixMessage_2, 
+										invalidFixMessage_3, invalidFixMessage_4, invalidFixMessage_5,
+										invalidFixMessage_6, invalidFixMessage_7
+									}; 
+		
+		int fixMsgCount = Runtime.getRuntime().availableProcessors();
+		FixMessagePool fmp = new FIXMessageBuilder().initFixMessagePool(true, fixMsgCount);
+		FixMessageContainer<FixMessage> fmc = fmp.getFixMessageContainer();
+		FixMessage fm =  fmc.getFixMessage();
+		FixMessageReader fmr = new FixMessageReader();
+		
+		NativeBytes nativeBytes = null;
+		byte [] msgBytes = null;
+		ByteBufferBytes byteBufBytes = null;
+		
+		for (int i=0;i<fixMessagesArray.length;i++){
+			
+			nativeBytes = new DirectStore(fixMessagesArray[i].length()).bytes();
+			nativeBytes.write(fixMessagesArray[i].replace('|', '\u0001').getBytes());
+			msgBytes = fixMessagesArray[i].replace('|', '\u0001').getBytes();
+			byteBufBytes = new ByteBufferBytes(ByteBuffer.allocate(msgBytes.length).order(ByteOrder.nativeOrder()));
+			byteBufBytes.write(msgBytes);
+			fmr.setFixBytes(byteBufBytes);
+			try {
+				fmr.parseFixMsgBytes();
+				Field[] field = fmr.getField();
+				
+				for (int j=0;j<field.length;j++){
+		        	try {		        		
+						fm.getField(j).setFieldData(field[j].getFieldData());
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        } 
+				assertEquals(1, fm.isValid());
+				fm.reset();				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//checks the validity of this message
+			
+			
+		}
+		
+		
 	}
 	
 	
