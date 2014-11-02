942b2c0d71277c58e459a994ac0dacd7b77f4e35
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-11-01 22:07:34 -0400

    Update TestTransFix.java

--- fix-sandbox/include/src/test/java/net/openhft/fix/include/v42/examples/TestTransFix.java
+++ fix-sandbox/include/src/test/java/net/openhft/fix/include/v42/examples/TestTransFix.java
@@ -168,5 +168,81 @@ public class TestTransFix {
 		
 		
 	}
+	
+		
+	@Test
+	public void avgParseTime() throws Exception{
+		
+		String sampleFixMessage = "8=FIX.4.2|9=154|35=6|49=BRKR|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=FIA.MI|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		int fixMsgCount = Runtime.getRuntime().availableProcessors();
+		FixMessagePool fmp = new FIXMessageBuilder().initFixMessagePool(true, fixMsgCount);
+		FixMessageContainer<FixMessage> fmc = fmp.getFixMessageContainer();
+		FixMessage fm =  fmc.getFixMessage();
+		FixMessageReader fmr = new FixMessageReader(fm);		
+
+		byte [] msgBytes = sampleFixMessage.replace('|', '\u0001').getBytes();
+		ByteBufferBytes byteBufBytes = new ByteBufferBytes(ByteBuffer.allocate(msgBytes.length).order(ByteOrder.nativeOrder()));
+		byteBufBytes.write(msgBytes);
+		
+		int counter= 0;
+        int runs = 50000000;
+        long start = System.nanoTime();
+        for (int i = 0; i < runs; i++) { 
+        	fmr.setFixMessage(fm);
+        	fmr.setFixBytes(byteBufBytes);
+            fmr.parseFixMsgBytes();
+            counter++;
+        }
+        long time = System.nanoTime() - start;
+        System.out.printf("Average parse time was %.2f us, fields per message %.2f, for %.2fm iterations", time / runs / 1e3, (double) counter / runs, (int)runs/1e6 );
+	
+	}
+	
+	
+	@Test
+	public void avgSetAndReverseLookuptime() throws Exception{
+		String sampleFixMessage = "8=FIX.4.2|9=154|35=6|49=BRKR|56=INVMGR|34=238|52=19980604-07:59:56|23=115686|28=N|55=FIA.MI|54=2|27=250000|44=7900.000000|25=H|10=231|";
+		int fixMsgCount = Runtime.getRuntime().availableProcessors();
+		FixMessagePool fmp = new FIXMessageBuilder().initFixMessagePool(true, fixMsgCount);
+		FixMessageContainer<FixMessage> fmc = fmp.getFixMessageContainer();
+		FixMessage fm =  fmc.getFixMessage();
+		FixMessageReader fmr = new FixMessageReader(fm);		
+
+		byte [] msgBytes = sampleFixMessage.replace('|', '\u0001').getBytes();
+		ByteBufferBytes byteBufBytes = new ByteBufferBytes(ByteBuffer.allocate(msgBytes.length).order(ByteOrder.nativeOrder()));
+		byteBufBytes.write(msgBytes);
+		
+		int counter= 0;
+        int runs = 50000000;
+        ByteBufferBytes reverseLookupBuf = null;
+		byte[] readBytes = null;
+		int len = -1;
+		String bufData = null;
+		int [] fixFieldIds = {8,9,35,49,56,34,52,23,28,55,27,44,25,10};
+		fmr.setFixMessage(fm);
+    	fmr.setFixBytes(byteBufBytes);
+        fmr.parseFixMsgBytes();
+        long start = System.nanoTime();
+        
+        for (int i = 0; i < runs; i++) {             
+            
+    		for (int j=0;j<fixFieldIds.length;j++){  			
+    			
+    			//reading data from ByteBufferByte back for verification
+    			reverseLookupBuf = fm.getField( fixFieldIds[j] ).getFieldData();			
+    			reverseLookupBuf.flip();
+    			len = (int)(reverseLookupBuf.limit()- reverseLookupBuf.position());			
+    			readBytes = new byte[(int) len];			
+    			reverseLookupBuf.readFully(readBytes);    			
+    			
+//    			bufData = fm.getField(fixFieldIds[j]).getNumber()+"="+ new String (readBytes);    			
+//    			System.out.println(fixFieldIds[j]+ ": Reverse Look up-->"+ bufData);
+    		}            
+            counter++;
+        }
+        long time = System.nanoTime() - start;
+        System.out.printf("Average reverse-look-up time was %.2f us, for %.2fm iterations", time / runs / 1e3, (int)runs/1e6 );
+	
+	}
 
 }
