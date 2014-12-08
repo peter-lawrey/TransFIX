c0af1686ff622996f6117d86065f5824283098e4
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-12-07 19:27:17 -0500

    added comments

--- src/main/java/net/openhft/fix/include/util/FixMessagePool.java
+++ src/main/java/net/openhft/fix/include/util/FixMessagePool.java
@@ -15,15 +15,20 @@
  */
 
 package net.openhft.fix.include.util;
-import net.openhft.fix.include.v42.FixMessage;
-import net.openhft.lang.io.NativeBytes;
-
 import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import net.openhft.fix.include.v42.FixMessage;
+import net.openhft.lang.io.NativeBytes;
+
+/**
+ * Off-heap, lock-free FixMessage object pool. All checkouts from the pool are ThreadLocal-based and can be obtained 
+ * even before the threadlocal releases it. 
+ *
+ */
 public class FixMessagePool implements FixPoolFactory<FixMessage>{
 	
 	private FixMessageContainer<FixMessage>[] fixMessageArr;	
@@ -37,6 +42,15 @@ public class FixMessagePool implements FixPoolFactory<FixMessage>{
 	
 	private ThreadLocal<FixMessageContainer<FixMessage>> fixLocal = new ThreadLocal<FixMessageContainer<FixMessage>>();
 	
+	
+	/**
+	 * Constructor to create FixMessage objects and initialize the pool with default FixConfig. @param fixPoolFactory should be null 
+	 * for implementation of FixPoolFactory as this class. 
+	 * 
+	 * @param fixPoolFactory -factory used for create a new FixMessage Object
+	 * @param poolSize -usually, should equal to the number of processors on the machine
+	 * @param useDefault -instructs to create from FixConfig. Currently, only default=true implemented.
+	 */
 	@SuppressWarnings({ "unchecked", "restriction" })
 	public FixMessagePool(FixPoolFactory<FixMessage> fixPoolFactory , int poolSize, boolean useDefault){
 		
@@ -60,6 +74,11 @@ public class FixMessagePool implements FixPoolFactory<FixMessage>{
 		TAIL_ADJUSTMENT = 31 - Integer.numberOfLeadingZeros((int) ARR_INDEX);
 	}
 	
+	/**
+	 * Checks out an unused FixMessage object from the pool. The FixMessage object needs to be obtained from its
+	 * FixMessageContainer
+	 * @return FixMessageContainer<FixMessage>
+	 */
 	@SuppressWarnings("restriction")
 	public FixMessageContainer<FixMessage> getFixMessageContainer(){
 		int localTakePointer;
@@ -85,6 +104,10 @@ public class FixMessagePool implements FixPoolFactory<FixMessage>{
 		return null;		
 	}
 	
+	/**
+	 * @param fixMsgContainer -checks in this FixMessage object from FixMessageContainer.
+	 * @throws Exception
+	 */
 	@SuppressWarnings("restriction")
 	public void putFixMessageContainer(FixMessageContainer<FixMessage> fixMsgContainer) throws Exception{
 			int localPosition=objectPutPosition;			
@@ -98,6 +121,12 @@ public class FixMessagePool implements FixPoolFactory<FixMessage>{
 			}		
 	}
 	
+	/**
+	 * Static Inner class used as a container for FixMessage objects. This is used for maintaining the state of the 
+	 * FixMessage Object's state. The state of a FixMessage object can be either in-use OR available.  
+	 *
+	 * @param <FixMessage>
+	 */
 	@SuppressWarnings("hiding")
 	public static class FixMessageContainer<FixMessage> implements Externalizable{
 		private FixMessage fixMessage;
@@ -109,6 +138,9 @@ public class FixMessagePool implements FixPoolFactory<FixMessage>{
 			this.fixMessage = fixMessage;
 		}
 		
+		/**
+		 * @return FixMessage
+		 */
 		public FixMessage getFixMessage() {
 			return fixMessage;
 		}
@@ -126,6 +158,9 @@ public class FixMessagePool implements FixPoolFactory<FixMessage>{
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see net.openhft.fix.include.util.FixPoolFactory#create(boolean)
+	 */
 	@Override
 	public FixMessage create(boolean useDefault) 
 	{		
