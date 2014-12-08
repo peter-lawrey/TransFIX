546bd5159fcb4d754a187e6d1816283426af1489
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-12-07 19:24:45 -0500

    added comments

--- src/main/java/net/openhft/fix/include/util/FixConstants.java
+++ src/main/java/net/openhft/fix/include/util/FixConstants.java
@@ -15,6 +15,11 @@
  */
 package net.openhft.fix.include.util;
 
+/**
+ * Represents actual Fix 4.2 header/tailer/messages/fields in static arrays. This class is loaded during initialization of 
+ * <FixMessagePool> class and access to array elements is via index thereafter.  
+ *
+ */
 public class FixConstants {
 	
 	public final static String [] headerFieldName = {
