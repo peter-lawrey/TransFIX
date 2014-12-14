f0395e720fd075048c70ee71723dbd386dba15e1
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-12-14 14:15:59 -0500

    Update FixField.java

--- src/main/java/net/openhft/fix/model/FixField.java
+++ src/main/java/net/openhft/fix/model/FixField.java
@@ -17,16 +17,84 @@
 package net.openhft.fix.model;
 
 /**
- * Fix Field type as per FIX definition
- * @author Adam Rosenberger
+ * This enum determines the mapping of fix 4.2 datatypes to java primitives
+ * @author Adam Rosenberger/Anshul Shelley
  */
-public enum FieldType {
-    Mandatory,
-    Present,
-    Position,
-    Group,
-    Component,
-    Suppress,
-    Automatic,
-    Count
+public enum FixField implements FixFieldTypeInterface{
+    Int(DataType.Int),
+    Length(DataType.Long),
+    TagNum(DataType.Long),
+    SeqNum(DataType.Long),
+    NumInGroup(DataType.Long),
+    DayOfMonth(DataType.Int),
+
+    Double(DataType.Double),
+    Qty(DataType.Double),
+    Price(DataType.Double),
+    PriceOffset(DataType.Double),
+    Amt(DataType.Double),
+    Percentage(DataType.Double),
+
+    Char(DataType.Char),
+    Boolean(DataType.Boolean),
+
+    String(DataType.String),
+    MultipleCharValue(DataType.String),
+    MultipleStringValue(DataType.String),
+    Country(DataType.String),
+    Currency(DataType.String),
+    Exchange(DataType.String),
+    MonthYear(DataType.String),
+    UTCTimestamp(DataType.String),
+    UTCTimeOnly(DataType.String),
+    UTCDateOnly(DataType.String),
+    LocalMktDate(DataType.String),
+    TZTimeOnly(DataType.String),
+    TZTimestamp(DataType.String),
+    Data(DataType.String),
+    XMLData(DataType.String),
+    Language(DataType.String),
+
+    Tenor(DataType.Pattern),
+    Reserved100Plus(DataType.Pattern),
+    Reserved1000Plus(DataType.Pattern),
+    Reserved4000Plus(DataType.Pattern);
+
+    private final DataType dataType;
+
+    private FixField(DataType dataType) {
+        this.dataType = dataType;
+    }
+
+    public final boolean isInt() {
+        return dataType == DataType.Int;
+    }
+    
+    public final boolean isLong() {
+        return dataType == DataType.Long;
+    }
+
+    public final boolean isFloat() {
+        return dataType == DataType.Float;
+    }
+    
+    public final boolean isDouble() {
+        return dataType == DataType.Double;
+    }
+
+    public final boolean isChar() {
+        return dataType == DataType.Char;
+    }
+
+    public final boolean isString() {
+        return dataType == DataType.String;
+    }
+    
+    public final boolean isBoolean() {
+        return dataType == DataType.Boolean;
+    }
+
+    public final boolean isPattern() {
+        return dataType == DataType.Pattern;
+    }
 }
