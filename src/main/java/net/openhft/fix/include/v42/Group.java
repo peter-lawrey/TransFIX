3f705d167f9a7cfc53550b19922d028330dd9030
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-12-14 14:01:41 -0500

    Added Javadoc Comments

--- src/main/java/net/openhft/fix/include/v42/Group.java
+++ src/main/java/net/openhft/fix/include/v42/Group.java
@@ -23,6 +23,10 @@ import java.io.ObjectOutput;
 import net.openhft.lang.collection.HugeArray;
 import net.openhft.lang.collection.HugeCollections;
 
+/**
+ * Place holder for FIX Protocol object Group
+ *
+ */
 public class Group implements Externalizable
 {
 
