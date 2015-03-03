53afab4a4583fe4ef734585e769d5e710c50a669
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-03-03 22:54:01 +0000

    Rename peter and peter_2 to peter.lawrey

--- src/main/java/net/openhft/fix/model/FieldMetadata.java
+++ src/main/java/net/openhft/fix/model/FieldMetadata.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Peter Lawrey
+ * Copyright 2013 peter.lawrey Lawrey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
